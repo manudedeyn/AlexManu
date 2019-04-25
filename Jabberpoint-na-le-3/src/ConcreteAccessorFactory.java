7fa78008a3faf1f9003ffcfb43f3fd3b4278636e
Alex Walgreen <alexwalgreen@gmail.com>
2019-04-25 20:55:28 +0200

    Fix merge error

--- Jabberpoint-na-le-3/src/ConcreteAccessorFactory.java
+++ Jabberpoint-na-le-3/src/ConcreteAccessorFactory.java
@@ -22,26 +22,3 @@ public class ConcreteAccessorFactory extends AccessorFactory{
 		return values[values.length-1];
 	}
 }
-
-public class ConcreteAccessorFactory extends AccessorFactory{
-
-	@Override
-	public Accessor createAccessor(String fileName) {
-		switch(getExtension(getExtension(fileName))) {
-		case "xml":
-			return new XMLAccessor();
-		case "txt":
-			//TODO
-			return null;
-			//return new TxtAccessor();
-		}
-		
-		//DEFAULT
-		return new XMLAccessor();
-	}
-	
-	public String getExtension(String fileName) {
-		String[] values = fileName.split("\\.");
-		return values[values.length-1];
-	}
-}
