bccc9e70e58e4cc3367ce1dad2ea323d64e6ec91
Alex Walgreen <alexwalgreen@gmail.com>
2019-04-25 20:54:28 +0200

    Add file.

--- Jabberpoint-na-le-3/src/ConcreteAccessorFactory.java
+++ Jabberpoint-na-le-3/src/ConcreteAccessorFactory.java
@@ -17,6 +17,30 @@ public class ConcreteAccessorFactory extends AccessorFactory{
 	}
 	
 	public String getExtension(String fileName) {
+		
+		String[] values = fileName.split("\\.");
+		return values[values.length-1];
+	}
+}
+
+public class ConcreteAccessorFactory extends AccessorFactory{
+
+	@Override
+	public Accessor createAccessor(String fileName) {
+		switch(getExtension(getExtension(fileName))) {
+		case "xml":
+			return new XMLAccessor();
+		case "txt":
+			//TODO
+			return null;
+			//return new TxtAccessor();
+		}
+		
+		//DEFAULT
+		return new XMLAccessor();
+	}
+	
+	public String getExtension(String fileName) {
 		String[] values = fileName.split("\\.");
 		return values[values.length-1];
 	}
