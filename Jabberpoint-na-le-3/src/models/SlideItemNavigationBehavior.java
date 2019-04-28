956cc9a2da847592812d56c07ac550e0d9415fa1
manudedeyn <manu.dedeyn@gmail.com>
2019-04-28 17:07:42 +0200

    Finished backwards navigation

--- Jabberpoint-na-le-3/src/models/SlideItemNavigationBehavior.java
+++ Jabberpoint-na-le-3/src/models/SlideItemNavigationBehavior.java
@@ -26,7 +26,7 @@ public class SlideItemNavigationBehavior extends NavigationBehavior {
 			start = slide.getCurrentSlideItemNumber() + 1;	
 		}		
 					
-		nextLevel1Index = findNextLevel1Index(items, start + 1, 1);
+		nextLevel1Index = findNextLevelIndex(items, start + 1, 1);
 
 		if (!result) {
 			return false;
@@ -39,26 +39,75 @@ public class SlideItemNavigationBehavior extends NavigationBehavior {
 				break;
 			}
 		}
-		
-		return result;
+		if (start == nextLevel1Index) {
+			return false;
+		}
+		else {		
+			return result;
+		}
 	}
 
 	@Override
 	public boolean previous() {
-		Slide slide = super.adaptee.getCurrentSlide();
+	Slide slide = super.adaptee.getCurrentSlide();
 		
 		if (slide == null) {
 			return false;
 		}
 		
-		return slide.previousItem();
+		boolean result = true;
+		
+		Vector<SlideItem> items = slide.getSlideItems();
+		SlideItem currentItem = slide.getCurrentItem();
+		
+		int startIndex = 0;
+		int previousLevel1Index = 0;
+		
+		if (currentItem != null) {
+			startIndex = slide.getCurrentSlideItemNumber() + 1;	
+		}	
+					
+		previousLevel1Index = findPreviousLevelIndex(items, startIndex - 1, 1);
+
+		if (!result) {
+			return false;
+		}
+		
+		for(int j = startIndex; j > previousLevel1Index; j--) {
+			result = result && slide.previousItem();
+			
+			if (!result) {
+				break;
+			}
+		} 
+		
+		if (startIndex == previousLevel1Index) {
+			return false;
+		}
+		else {		
+			return result;
+		}
+		
+
 	}
 	
-	private int findNextLevel1Index(Vector<SlideItem> items, int startIndex, int level) {
+	private int findNextLevelIndex(Vector<SlideItem> items, int startIndex, int level) {
 		for (int i = startIndex, len = items.size(); i < len; i++) {
 			SlideItem item = items.elementAt(i);
 			
-			if (item.getLevel() == 1) {
+			if (item.getLevel() == level) {
+				return i;
+			}
+		}
+		
+		return items.size();
+	}
+	
+	private int findPreviousLevelIndex(Vector<SlideItem> items, int startIndex, int level) {
+		for (int i = startIndex; i >= 0; i--) {
+			SlideItem item = items.elementAt(i);
+			
+			if (item.getLevel() == level) {
 				return i;
 			}
 		}
