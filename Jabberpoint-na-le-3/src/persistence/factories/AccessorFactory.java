b53c80466e548761d819ca8ddd496d820d88d1fa
Alex Walgreen <alexwalgreen@gmail.com>
2019-04-28 12:02:32 +0200

    Move DemoPresentation to its own class, move some code from JabberPoint to PersistenceController, removed DemoPresentation accessor.

--- Jabberpoint-na-le-3/src/persistence/factories/AccessorFactory.java
+++ Jabberpoint-na-le-3/src/persistence/factories/AccessorFactory.java
@@ -1,9 +1,16 @@
 package persistence.factories;
 
+import models.factories.DefaultNavigationBehaviorFactory;
+import models.factories.NavigationBehaviorFactory;
 import persistence.Accessor;
 
 //This is the AbstractFactory class
 
 public abstract class AccessorFactory {
+	public static final AccessorFactory getInstance() {
+		// Just return the default implementation. We should get the actual type by means of configuration instead.
+		return new DefaultAccessorFactory();		
+	}
+	
 	public abstract Accessor createAccessor(String fileName);
 }
